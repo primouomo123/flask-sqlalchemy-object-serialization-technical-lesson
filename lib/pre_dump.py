fbceadbeb21e0b5bdb991fd087833e5b3a248482
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-02-14 16:34:22 -0500

    final solution

--- lib/pre_dump.py
+++ lib/pre_dump.py
@@ -10,24 +10,30 @@ class Album():
         self.title = title
         self.artist = artist
         self.num_sold = num_sold
-    
-# schema 
+
+# schema
 
 class AlbumSchema(Schema):
-    title = fields.String()
-    artist = fields.String()
-    num_sold = fields.Int()
-    
-    
+    title = fields.String(required=True)
+    artist = fields.String(required=True)
+    num_sold = fields.Int(required=True)
+    big_hit = fields.Boolean(dump_only = True)
+
+    # compute field prior to serialization
+    @pre_dump()
+    def get_data(self, data, **kwargs):
+        data.big_hit = data.num_sold > 1000000
+        return data
+
 # create model and schema instances
 album_1 = Album("The Wall", "Pink Floyd", 19000000)
 album_2 = Album("Renaissance", "Beyonce", 332000)
-schema = AlbumSchema() 
+schema = AlbumSchema()
 
 # deserialize model instances
 
 pprint(schema.dumps(album_1))
-# => '{"title": "The Wall", "artist": "Pink Floyd", "num_sold": 19000000}'
+# => '{"title": "The Wall", "artist": "Pink Floyd", "num_sold": 19000000, "big_hit": true}'
 
 pprint(schema.dumps(album_2))
-# => '{"title": "Renaissance", "artist": "Beyonce", "num_sold": 332000}'
+# => '{"title": "Renaissance", "artist": "Beyonce", "num_sold": 332000, "big_hit": 'false}'
